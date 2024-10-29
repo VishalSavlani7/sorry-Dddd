f982a2191703e025077cb28abce9106946fe4b7c
Vsavlani7 <visshalsavlani44@gmail.com>
2024-10-29 09:15:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import sampleImage from "./images/sorry.jpg";
+import sample2Image from "./images/plss.jpeg";
 
 function App() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleYesClick = () => {
+    alert("Thank You so much Dddddd....I LOVE YOU");
+  };
+
+  const handleNoClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        textAlign: "center",
+        paddingTop: "50px",
+        backgroundColor: "pink",
+        height: "43rem",
+      }}
+    >
+      <img
+        src={sampleImage} // Use the imported image here
+        alt="Sample"
+        style={{ marginBottom: "20px" }}
+      />
+      <div>
+        <button onClick={handleYesClick} style={{ padding: "10px" }}>
+          Yes
+        </button>
+        <button
+          onClick={handleNoClick}
+          style={{ marginLeft: "20px", padding: "10px" }}
         >
-          Learn React
-        </a>
-      </header>
+          No
+        </button>
+      </div>
+
+      {showPopup && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "300px",
+              padding: "20px",
+              backgroundColor: "white",
+              borderRadius: "8px",
+              textAlign: "center",
+            }}
+          >
+            <img
+              src={sample2Image} // Use the imported image here
+              alt="Sample"
+              style={{ marginBottom: "20px" }}
+            />
+            <button onClick={handleClosePopup}>Back</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
